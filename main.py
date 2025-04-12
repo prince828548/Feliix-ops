7dbee099622e0e1580717407f14817a70a27c103
Feliix-ops <kishorkannake866@gmail.com>
2025-04-12 17:54:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def send_message():
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
     body{
-      background-color: https://imgur.com/a/PJDrGGr}
+      background-color: IMG_20250408_085449_617}
     .container{
       max-width: 300px;
       background-color: bisque;
